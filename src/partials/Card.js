f36ca47e15e381f69cf1c2c96f167c7f68b1ec55
hasobi <roid.radityo11@gmail.com>
2020-10-19 09:02:28 +0700

    first commit

--- src/partials/Card.js
+++ src/partials/Card.js
@@ -16,25 +16,25 @@ function Card() {
         </div>
         <div className="text-center mt-5">
           <p className="text-xl sm:text-2xl font-semibold text-gray-900">
-            Brayden W
+            Hasobi R. Radityo
           </p>
           <p className="text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block border-b-2">
-            Front-End Developer/Designer
+            DevOps Engineer
           </p>
           <div className="flex align-center justify-center mt-4">
-            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/BraydenTW">
+            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/hasobi">
               <FaGithub />
               <span class="sr-only">Github</span>
             </a>
-            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-pink-600 hover:bg-pink-600 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/BraydenTW">
+            {/* <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-pink-600 hover:bg-pink-600 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/BraydenTW">
               <FaDribbble />
               <span class="sr-only">Dribble</span>
-            </a>
-            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-500 hover:bg-blue-500 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/BraydenTW">
+            </a> */}
+            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-blue-500 hover:bg-blue-500 rounded-full hover:text-white transition-colors duration-300" href="https://twitter.com/hasobi">
               <FaTwitter />
               <span class="sr-only">Twitter</span>  
             </a>
-            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-500 hover:bg-teal-500 rounded-full hover:text-white transition-colors duration-300" href="https://github.com/BraydenTW">
+            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-teal-500 hover:bg-teal-500 rounded-full hover:text-white transition-colors duration-300" href="mailto:hasobi.works@gmail.com">
               <FaRegEnvelope />
               <span class="sr-only">Email</span>  
             </a>
